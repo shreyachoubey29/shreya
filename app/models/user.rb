97b431519ac33a22ec7ab7eae8c013350f6886bf
Shreya Choubey <choubeyshreya0@gmail.com>
2017-02-10 10:07:36 +0000

    Add password and attributes to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true,     
     uniqueness:{case_sensitive: false}, length: {maximum: 105 } ,
     format: {with: VALID_EMAIL_REGEX}
-
+    has_secure_password
   
 end
 
